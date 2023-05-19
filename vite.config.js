70f6addc6a7d372f1f0785f6e5be70e8e494ae10
Lucas al Jalali <107360413+lucasaljalali@users.noreply.github.com>
2023-05-19 12:03:09 +0100

    FIX: vite env for vercel

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command, mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-
-  return {
-    plugins: [react()],
-    define: {
-      'process.env.REACT_APP_APP_URL': JSON.stringify(env.REACT_APP_APP_URL)
-    }
-  };
-});
+export default defineConfig({
+  plugins: [react()],
+})
