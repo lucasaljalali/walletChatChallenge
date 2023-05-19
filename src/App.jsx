6b8566342048d40b8a72f85df82223d4c6e21dab
Lucas al Jalali <107360413+lucasaljalali@users.noreply.github.com>
2023-05-19 09:54:38 +0100

    IMPLEMENT: challenge

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import {WalletChatWidget} from 'react-wallet-chat-v0'
+import 'react-wallet-chat-v0/dist/index.css'
+import './App.css'
+
+export default function App() {
+
+  return (
+    <div className="mainContainer">
+      <h1 className="mainTitle">Lucas Al Jalali`s Challenge</h1>
+      <WalletChatWidget />
+    </div>
+  )
+}
