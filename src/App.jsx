31683204615b90731ed7f69091e10e72762b5da5
Lucas al Jalali <107360413+lucasaljalali@users.noreply.github.com>
2023-05-19 10:14:43 +0100

    TEST: chat with owner

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
+import { useState } from 'react'
 import {WalletChatWidget} from 'react-wallet-chat-v0'
 import 'react-wallet-chat-v0/dist/index.css'
 import './App.css'
 
 export default function App() {
-
+  const [widgetState, setWidgetState] = useState({})
   return (
     <div className="mainContainer">
       <h1 className="mainTitle">Lucas Al Jalali`s Challenge</h1>
-      <WalletChatWidget />
+      <WalletChatWidget widgetState={widgetState}/>
+      <div title="Chat With Owner">
+        <button
+          onClick={() => {
+              setWidgetState(
+              {
+                 ...widgetState, 
+                chatAddr: '0x17FA0A61bf1719D12C08c61F211A063a58267A19',
+                isOpen: true
+              }
+            )
+            }
+          }
+        />
+      </div>
     </div>
   )
 }
